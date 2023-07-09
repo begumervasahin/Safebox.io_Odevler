6fa755e9fc5041915171aa63e86cb5340e879939
begumervasahin <93603693+begumervasahin@users.noreply.github.com>
2023-07-09 16:56:20 +0300

    Bölüm 1 Bitirme Projesi
    Cyber Security Data Science Eğitimi Bölüm 1 Bitirme Projesi

---
+++ 6)Cyber Security Data Science Eğitimi Bölüm 1 Bitirme Projesi/Todo-App-FullStack/backend/app.py
@@ -0,0 +1,81 @@
+from flask import Flask , request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from flask_cors import CORS
+
+#data bağlantısını yaptığım yer kendi paswordunuzu girmeyi unutmayın isteklerin kontrlunü postman den yapmanız tavsiye edilir
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:şifre@localhost:5433/todo-app-fullstack'
+db = SQLAlchemy(app)
+CORS(app)
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Event: {self.description}"
+
+    def __init__(self, description):
+        self.description = description
+
+# Etkinliğin formatlı halini döndürür
+def format_event(event):
+    return {
+        "description": event.description,
+        "id": event.id,
+        "created_at": event.created_at,
+    }   
+
+@app.route('/')
+def hello():
+    return 'Hello safebox !'
+
+# Yeni bir etkinlik oluşturur
+@app.route('/events', methods=['POST'])
+def create_event():
+    description = request.json['description']
+    event = Event(description)
+    db.session.add(event)
+    db.session.commit()
+    return format_event(event)
+
+  # Tüm etkinlikleri getirir
+@app.route('/events', methods=['GET'])
+def get_events():
+    events = Event.query.order_by(Event.id.asc()).all()
+    event_list = []
+    for event in events:
+        event_list.append(format_event(event))
+    return{'events':event_list}    
+
+  # Belirli bir etkinliği getirir
+@app.route('/events/<id>', methods=['GET'])
+def get_event(id):
+    event = Event.query.filter_by(id=id).one()
+    formatted_event=format_event(event)
+    return {'event':formatted_event}
+
+  # Belirli bir etkinliği siler
+@app.route('/events/<id>', methods=['DELETE'])
+def delete_event(id):
+     event = Event.query.filter_by(id=id).one()
+     db.session.delete(event)
+     db.session.commit()
+     return f'(id: {id}) silindi'
+
+ # Belirli bir etkinliği günceller
+@app.route('/events/<id>', methods=['PUT'])
+def update_event(id):
+    event=Event.query.filter_by(id=id)
+    description=request.json['description']
+    event.update(dict(description=description, created_at=datetime.utcnow()))
+
+    db.session.commit()
+    return {'event': format_event(event.one())}
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run()
